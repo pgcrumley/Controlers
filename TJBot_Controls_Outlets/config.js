6931bce1bdd25f99bc14c11a7bab99ba0a575510
Paul G Crumley <pgcrumley@gmail.com>
2020-11-08 19:39:44 -0500

    add EtekCity

---
+++ TJBot_Controls_Outlets/config.js
@@ -0,0 +1,19 @@
+/*
+User-specific configuration
+    ** IMPORTANT NOTE ********************
+    * Please ensure you do not interchange your username and password.
+    * Hint: Your username is the lengthy value ~ 36 digits including a hyphen
+    * Hint: Your password is the smaller value ~ 12 characters
+*/
+
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+  username: "ac8e2ee3-8972-4519-8f00-97458e909acd",
+  password: "N7bS73ynPWSw"
+}
+
+exports.address = 80; // set as needed for devices
